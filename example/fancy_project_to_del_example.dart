c2a11c01baed0fad1e968148c10e1bd11e89ffd3
julyan cm <jny22@example.com>
2020-02-01 23:40:13 +0100

    init-project

---
+++ example/fancy_project_to_del_example.dart
@@ -0,0 +1,6 @@
+import 'package:fancy_project_to_del/fancy_project_to_del.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/fancy_project_to_del.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library fancy_project_to_del;
+
+export 'src/fancy_project_to_del_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
